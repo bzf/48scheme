c1f2c95d16500969ac747103390365fb988f4437
André Ligné <hello@bzf.se>
2016-08-09 11:11:56 +0200

    chapter 5: implement a read-eval-print-loop

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 
+import System.IO
 import Numeric
 import Data.Char hiding (isNumber, isSymbol)
 
@@ -331,9 +332,33 @@ readExpr input = case parse parseExpr "lisp" input of
                    Left err -> throwError $ Parser err
                    Right value -> return value
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+     then return ()
+     else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "48scheme> ") evalAndPrint
+
 
 main :: IO ()
 main = do
   args <- getArgs
-  evaled <- return $ liftM show (readExpr (args !! 0) >>= eval)
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ args !! 0
+    otherwise -> putStrLn "Program takes only 0 or 1 arguments"
